0ad5b3f1a924f546e3d0b21215938680bfa3b056
Luke Chou <luke.y.chou@gmail.com>
2015-03-12 19:37:54 -0500

    attempted to create driver for TB6612FNG and failed

--- Rasware2013/RASDemo/Main.c
+++ Rasware2013/RASDemo/Main.c
@@ -2,35 +2,18 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/motor.h>
 
-static tMotor *Motors[4];
-static tBoolean initialized = false;
 tBoolean blink_on = true;
 
-void blink(void) {
+void blink(void)
+{
     SetPin(PIN_F3, blink_on);
     blink_on = !blink_on;
 }
 
-void initMotors(void)
-{
-	 if (!initialized)
-	 {
-		 initialized = true;
-	         Motors[0] = InitializeTLEMotor(PIN_B4, PIN_B5, true, false);	        
-		 Motors[1] = InitializeTLEMotor(PIN_E4, PIN_E5, true, false);
-	 }
-}
 
-float forward = 1.00;
-float back = -0.75;
-float adjustValue = 0.25; //why do we have this?
 int main(void)
 {
-    CallEvery(blink, 0, 0.5);
-   // initMotors();
-
-//	SetMotor(Motors[0], forward);
-//	SetMotor(Motors[1], forward);
+    	CallEvery(blink, 0, 0.5);
 	SetPin(PIN_B4,1);
 	SetPin(PIN_B5,0);
 	SetPin(PIN_E4,1);
