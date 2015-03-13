8e58ab6080fc7d9918ba2a2ef080ddf93f8f3f84
Michelle Chou <brunhin@gmail.com>
2015-03-12 21:30:31 -0500

    Fixed changes in Hurman....

--- Hurmanurmanur2015/ToshibaMotorDriver.h
+++ Hurmanurmanur2015/ToshibaMotorDriver.h
@@ -3,29 +3,11 @@
 #include <RASLib/inc/motor.h>
 #include <RASLib/inc/pwm.h>
 
-static tMotor *Motors[4];
-static tBoolean initialized = false;
-tBoolean blink_on = true;
-static tPWM *PWM_Left;
-static tPWM *PWM_Right;
-
-// heartbeat
-void blink(void) {
-    SetPin(PIN_F3, blink_on);
-    blink_on = !blink_on;
-}
-
 // pins for Toshiba motor driver
 #define L1 PIN_B4
 #define L2 PIN_B5 
 #define R1 PIN_E4 
 #define R2 PIN_E5
-
-// duty cycles for driving 
-float forward = 50;
-float left = 50;
-float right = 50;
-float back = 50;
 
 // TOSHIBA MOTOR DRIVER 
 // requires 2 "logic" pins for motor state
@@ -77,23 +59,3 @@ void TBBackward(float speed) {
 	// activate PWMs with speed
 }
 
-
-
-
-int main(void)
-{
-//	initMotors();
-		PWM_Left InitializePWM(PIN_B6, 1000);
-		PWM_Right InitializePWM(PIN_E6, 1000);
-	
-//	SetMotor(Motors[0], forward);
-//	SetMotor(Motors[1], forward);	
-//	SetPin(PIN_B4,1);
-//	SetPin(PIN_B5,0);
-//	SetPin(PIN_E4,1);
-//	SetPin(PIN_E5,0);
-	
-	TBForward(forward, forward, PWM_Left, PWM_Right);
-	
-	while(1);
-}
