65564cd314d26ad44cbd730794cc31e04c3316f1
David Park <david.park@utexas.edu>
2015-03-13 21:10:00 -0500

    back to Judd's old code

--- Rasware2013/RASDemo/Main.c
+++ Rasware2013/RASDemo/Main.c
@@ -17,13 +17,10 @@ tBoolean blink_on = true;
 void blink(void) {
     SetPin(PIN_F3, blink_on);
     blink_on = !blink_on;
+    SetPin(PIN_F2, blink_on);
 }
-
 int main(void) {
 
-<<<<<<< HEAD
-	CallEvery(blink, 0, 0.5);
-=======
 	CallEvery(blink, 0, 0.5);	
 
 // Initialize PWMs on Toshiba Driver
@@ -52,7 +49,6 @@ int main(void) {
 	Wait(1.0);
 	TBRight(100, 100);
 	Wait(1.0);
->>>>>>> c47b4abf26dee75d6fe2d790936e2bb83a2a076c
 	
 	// now slalom
 	for (int j = 0; j < 10; j += 1) {
@@ -62,12 +58,6 @@ int main(void) {
 		Wait(0.5);
 	}
 	
-<<<<<<< HEAD
-	//pwm_left = InitializePWM(PIN_B0, 1000);
-	//pwm_right = InitializePWM(PIN_B1, 1000);
-	
-	//TBForward(50, 50, pwm_left, pwm_right);
-=======
 	TBBackward(50, 50);
 	Wait(1.5);
 	TBBackward(100, 100);
@@ -79,27 +69,6 @@ int main(void) {
 	TBForward(100, 100);
 	Wait(1.5);
 	TBBrake();
->>>>>>> c47b4abf26dee75d6fe2d790936e2bb83a2a076c
 	
-	//while(1);
-
-	char input;
-
-	while(true) {
-		scanf("%c", input);
-		switch(input) {
-			case 'w':
-				//make arm go up
-
-				break;
-			case 's':
-				//make arm go down
-
-				break;
-			default:
-				//arm does nothing
-
-		}
-	}
+	while(1);
 }
-
