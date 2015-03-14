c47b4abf26dee75d6fe2d790936e2bb83a2a076c
Zach Dewey <zdewey413@gmail.com>
2015-03-14 18:11:18 -0500

    Some arm code hopefully

---
+++ Hurmanurmanur2015/Main.c
@@ -0,0 +1,59 @@
+#include "ToshibaMotorDriver.h"
+#include "RASLib/inc/common.h"
+#include "RASLib/inc/motor.h"
+#include "RASLib/inc/time.h"
+#include "RASLib/inc/pwm.h"
+#include "RASLib/inc/servo.h"
+
+// duty cycles for driving 
+float forward = 50;
+float left = 50;
+float right = 50;
+float back = 50;
+
+tBoolean blink_on = true;
+static tPWM *pwm_left;
+static tPWM *pwm_right;
+
+static tServo *armServo;
+static tServo *handServo;
+
+// heartbeat
+void blink(void) {
+    SetPin(PIN_F3, blink_on);
+    blink_on = !blink_on;
+}
+
+void moveArm(void)
+{
+	SetServo(handServo,0.5f);
+	SetServo(armServo, 1.0f);
+	Wait(1.0);
+	SetServo(handServo, 0);
+	SetServo(armServo, 0);
+}
+
+int main(void) {
+
+		CallEvery(blink, 0, 0.5);
+	
+// This works, driving forward only
+// ** PWM set to high **
+//	SetPin(PIN_B4,1);
+//	SetPin(PIN_B5,0);
+//	SetPin(PIN_E4,1);
+//	SetPin(PIN_E5,0);
+
+// Testing Toshiba Driver	
+	
+	pwm_left = InitializePWM(PIN_B6, 1000);
+	pwm_right = InitializePWM(PIN_E6, 1000);
+	armServo = InitializeServo(PIN_B2);
+	handServo = InitializeServo(PIN_B3);
+	
+	
+	TBForward(50, 50, pwm_left, pwm_right);
+	
+	while(1);
+}
+
