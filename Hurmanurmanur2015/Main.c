4ac5204833030e048ce1781d2b05fcb0950a0995
David Park <david.park@utexas.edu>
2015-04-03 19:46:17 -0500

    arm grabs ball and drops

--- Hurmanurmanur2015/Main.c
+++ Hurmanurmanur2015/Main.c
@@ -24,36 +24,95 @@ void blink(void) {
     blink_on = !blink_on;
 }
 
-void moveArm(void)
+void grabBall(void)
 {
 	//hand servo info
 	//0.55 = closed
 	//0.2 = open
 
-	SetServo(armServo, 0.75f);
+	int i;
+
 	SetPin(PIN_F2, blink_on);
 	SetPin(PIN_F3, !blink_on);
+
+	//set arm servo down after initialization
+	for(i = 50; i>=10; i -= 1)
+	{
+		SetServo(armServo, (float) i * 0.01);
+		Wait(0.1);
+	}
 	Wait(2.0);
 
-	SetServo(handServo, 0.2f);
+	//Open hand
 	SetPin(PIN_F3, blink_on);
 	SetPin(PIN_F3, !blink_on);
+	SetServo(handServo, 0.2f);
 	Wait(2.0);
 
-	SetServo(armServo, 0.1f);
+	//arm goes up
 	SetPin(PIN_F2, blink_on);
 	SetPin(PIN_F3, !blink_on);
+	for(i = 10; i<=100; i += 1)
+	{
+		SetServo(armServo, (float) i * 0.01);
+		Wait(0.05);
+	}
 	Wait(2.0);
 
-	SetServo(handServo, 0.55f);
+	//Close hand
 	SetPin(PIN_F3, blink_on);
 	SetPin(PIN_F2, !blink_on);
+	SetServo(handServo, 0.70f);
 	Wait(2.0);
+
+	
+	//arm goes down
+	SetPin(PIN_F2, blink_on);
+	SetPin(PIN_F3, !blink_on);
+	SetServo(armServo, 0.1);
 
 	//SetServo(armServo, 0.0f);
 	//SetPin(PIN_F2, blink_on);
 	//SetPin(PIN_F3, !blink_on);
 	//Wait(2.0);
+
+}
+
+void dropBall(void)
+{
+	int i;
+
+	//move arm up halfway
+	SetPin(PIN_F2, blink_on);
+	SetPin(PIN_F3, !blink_on);
+	for(i = 10; i<=55; i += 1)
+	{
+		SetServo(armServo, (float) i * 0.01);
+		Wait(0.05);
+	}
+	Wait(2.0);	
+
+	//open hand
+	SetPin(PIN_F3, blink_on);
+	SetPin(PIN_F3, !blink_on);
+	SetServo(handServo, 0.2f);
+	Wait(5.0);
+
+	//close hand
+	SetPin(PIN_F3, blink_on);
+	SetPin(PIN_F2, !blink_on);
+	SetServo(handServo, 0.70f);
+	Wait(2.0);
+
+	//arm goes down
+	SetPin(PIN_F2, blink_on);
+	SetPin(PIN_F3, !blink_on);
+	for(i = 55; i >= 10; i -= 1)
+	{
+		SetServo(armServo, (float) i * 0.01);
+		Wait(0.05);
+	}
+	SetServo(armServo, 0.1);
 }
 
 int main(void) {
@@ -73,13 +132,15 @@ int main(void) {
 	//pwm_right = InitializePWM(PIN_E6, 1000);
 	armServo = InitializeServo(PIN_B2);
 	handServo = InitializeServo(PIN_B3);
-	
-	
+
+	grabBall();	
+	Wait(3.0);
+	dropBall();
+
 	//TBForward(50, 50, pwm_left, pwm_right);
 	
 	//while(1);
 
-	moveArm();
 	//while(1) __asm("");
 
 /*
